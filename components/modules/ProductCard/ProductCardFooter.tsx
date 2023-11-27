cfc1ccb8f43c5a45f8610f85fccf518dc2d529aa
Sebastian Machaj <team@blackredstudio.com>
2023-11-27 11:51:45 +0100

    Product Card Btns fix

--- components/modules/ProductCard/ProductCardFooter.tsx
+++ components/modules/ProductCard/ProductCardFooter.tsx
@@ -18,20 +18,33 @@ function ProductCardFooter({
     handleDeleteProductFromList,
     currentUserProduct,
 }: TProductCardFooterProps) {
+    let prevStatus: 'visible' | 'invisible' = 'invisible';
+
+    if (
+        currentUserProduct?.status === 'visible' ||
+        currentUserProduct?.status === 'draftVisible'
+    ) {
+        prevStatus = 'visible';
+    }
+
     return (
         <div className="mt-8 space-y-4">
             {loading && <Loader className="mt-4" />}
             <Button
                 className="w-full"
-                onClick={() => handleAddProduct('invisible')}>
+                onClick={() => handleAddProduct(prevStatus)}>
                 Zapisz ocenę
             </Button>
-            <Button
-                className="w-full"
-                onClick={() => handleAddProduct('visible')}
-                variant={'orange'}>
-                Zapisz ocenę i dodaj do mojej listy
-            </Button>
+            {!currentUserProduct ||
+            currentUserProduct?.status === 'invisible' ||
+            currentUserProduct?.status === 'draft' ? (
+                <Button
+                    className="w-full"
+                    onClick={() => handleAddProduct('visible')}
+                    variant={'orange'}>
+                    Zapisz ocenę i dodaj do mojej listy
+                </Button>
+            ) : null}
             {currentUserProduct?.status === 'visible' ||
             currentUserProduct?.status === 'draftVisible' ? (
                 <Button
