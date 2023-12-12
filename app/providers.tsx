89e5ee0dcb6778bdb35442fee9a115a328be3479
Sebastian Machaj <team@blackredstudio.com>
2023-12-12 19:38:12 +0100

    add: handle request from google OAUTH

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,27 @@
 'use client';
 
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { SessionProvider } from 'next-auth/react';
+import { App as CapacitorApp, URLOpenListenerEvent } from '@capacitor/app';
+import { useRouter } from 'next/navigation';
 
 type TProviderLayoutProps = {
     children: ReactNode;
 };
 
 function Providers({ children }: TProviderLayoutProps) {
+
+    const router = useRouter();
+
+    useEffect(() => {
+        CapacitorApp.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
+            if(event.url.indexOf('api/auth/callback/google') > -1) {
+
+                const slug = event.url.split('mniam-mniam.vercel.app').pop() || '/';
+                router.push(slug);
+            }
+          });
+    }, []);
 
     return <SessionProvider>{children}</SessionProvider>;
 }
