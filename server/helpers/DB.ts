608e5bd105d261acd8fb57948cb79180073cbb52
Sebastian Machaj <team@blackredstudio.com>
2024-08-08 13:47:26 +0200

    fix

--- server/helpers/DB.ts
+++ server/helpers/DB.ts
@@ -1,32 +1,40 @@
 import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
+
 import * as schema from '../schemas';
 
 // Singleton function to ensure only one db instance is created
-async function singleton<Value>(
-    name: string,
-    value: () => Value,
-): Promise<Value> {
-    const globalAny: any = global;
-    globalAny.__singletons = globalAny.__singletons || {};
+// function singleton<Value>(
+//     name: string,
+//     value: () => Value,
+// ): Promise<Value> {
+//     const globalAny: any = global;
+//     globalAny.__singletons = globalAny.__singletons || {};
 
-    if (!globalAny.__singletons[name]) {
-        globalAny.__singletons[name] = await value();
-    }
+//     if (!globalAny.__singletons[name]) {
+//         globalAny.__singletons[name] = value();
+//     }
 
-    return globalAny.__singletons[name];
-}
+//     return globalAny.__singletons[name];
+// }
 
-// Function to create the database connection and apply migrations if needed
-async function createDatabaseConnection() {
-    const client = createClient({
-        url: process.env.TURSO_URL!,
-        authToken: process.env.TURSO_AUTH_TOKEN,
-    });
+// // Function to create the database connection and apply migrations if needed
+// function createDatabaseConnection() {
+//     const client = createClient({
+//         url: process.env.TURSO_URL!,
+//         authToken: process.env.TURSO_AUTH_TOKEN,
+//     });
 
-    return drizzle(client, { schema });
-}
+//     return drizzle(client, { schema });
+// }
 
-const DB = await singleton('DB', createDatabaseConnection);
+// const DB = singleton('DB', createDatabaseConnection);
+
+const client = createClient({
+    url: process.env.TURSO_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN,
+});
+
+const DB = drizzle(client, { schema });
 
 export { DB, schema };
